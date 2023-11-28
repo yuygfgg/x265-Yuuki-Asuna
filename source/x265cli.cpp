3da4b4110ec98d358701021b02209e630ac7f3af
Mr-Z-2697 <74594146+Mr-Z-2697@users.noreply.github.com>
2023-11-28 10:15:40 +0800

    vq3: add lookahead slices constraint

--- source/x265cli.cpp
+++ source/x265cli.cpp
@@ -141,7 +141,7 @@ namespace X265_NS {
         H0("-t/--tune <string>               Tune the settings for a particular type of source or situation:\n");
         H0("                                 psnr, ssim, grain, zerolatency, fastdecode,\n");
         H0("                                 f~ffffffffffffffffff(18), e~eeeeeeeeeeeee(13), (sane range is e3~f7),\n");
-        H0("                                 vq1~vq3(vq1 if no trailing number(i.e. vq=vq1)), none\n");
+        H0("                                 vq1~vq3(vq1 if no trailing number(i.e. --tune vq == --tune vq1)), none\n");
         H0("\nQuad-Tree size and depth:\n");
         H0("-s/--ctu <64|32|16>              Maximum CU size (WxH). Default %d\n", param->maxCUSize);
         H0("   --min-cu-size <64|32|16|8>    Minimum CU size (WxH). Default %d\n", param->minCUSize);
