f456abb04f6fa3433a0146f6e68aa9838a414608
Mr-Z-2697 <74594146+Mr-Z-2697@users.noreply.github.com>
2025-01-08 00:51:50 +0800

    correct resolution constraint and allow override
    probably should consider CU size as well but whatever

--- source/common/param.cpp
+++ source/common/param.cpp
@@ -1888,7 +1888,8 @@ int x265_check_params(x265_param* param)
 {
 #define CHECK(expr, msg) check_failed |= _confirm(param, expr, msg)
     int check_failed = 0; /* abort if there is a fatal configuration problem */
-    CHECK((param->sourceWidth > 8192 && param->sourceHeight > 4320), "Input video resolution exceeds the maximum supported 8K resolution of 8192x4320");
+    // TODO: add more Levels? 6.3-7.2, new in H.265 V9 (202309) and above
+    CHECK((param->sourceWidth * param->sourceHeight) > (8192 * 4352) && !param->bAllowNonConformance, "Input video resolution exceeds the maximum supported luma samples 35,651,584 (8192x4352) of Level 6.2, to override this limit, use --allow-non-conformance.");
     CHECK(param->uhdBluray == 1 && (X265_DEPTH != 10 || param->internalCsp != 1 || param->interlaceMode != 0),
         "uhd-bd: bit depth, chroma subsample, source picture type must be 10, 4:2:0, progressive");
     CHECK(param->maxCUSize != 64 && param->maxCUSize != 32 && param->maxCUSize != 16,
