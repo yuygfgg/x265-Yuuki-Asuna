8f8aba633d09dbf45a93f467148f1d3efd55a3a6
Mr-Z-2697 <74594146+Mr-Z-2697@users.noreply.github.com>
2025-05-02 22:17:21 +0800

    使hme参数info的语法更加相符

--- source/common/param.cpp
+++ source/common/param.cpp
@@ -2544,7 +2544,7 @@ char *x265_param2string(x265_param* p, int padx, int pady)
     BOOL(p->bEnableHME, "hme");
     if (p->bEnableHME)
     {
-        s += snprintf(s, bufSize - (s - buf), " hme-level-0,1,2=%d,%d,%d", p->hmeSearchMethod[0], p->hmeSearchMethod[1], p->hmeSearchMethod[2]);
+        s += snprintf(s, bufSize - (s - buf), " hme-search-L0,L1,L2=%d,%d,%d", p->hmeSearchMethod[0], p->hmeSearchMethod[1], p->hmeSearchMethod[2]);
         s += snprintf(s, bufSize - (s - buf), " hme-range-L0,L1,L2=%d,%d,%d", p->hmeRange[0], p->hmeRange[1], p->hmeRange[2]);
     }
     else
