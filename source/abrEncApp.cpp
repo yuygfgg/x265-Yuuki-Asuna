cb2849fd8596c72bdc0ed878b99f7710192dbaee
Mr-Z-2697 <74594146+Mr-Z-2697@users.noreply.github.com>
2025-06-10 01:07:31 +0800

    ifdef the filters codes

--- source/abrEncApp.cpp
+++ source/abrEncApp.cpp
@@ -255,6 +255,7 @@ namespace X265_NS {
             }
         }
 
+#ifdef ENABLE_ZIMG
         for (auto &&i : m_cliopt.filters)
         {
             i->setParam(m_param);
@@ -263,6 +264,7 @@ namespace X265_NS {
                 return -1;
             }
         }
+#endif
 
         /* This allows muxers to modify bitstream format */
         m_cliopt.output->setParam(m_param);
@@ -1210,6 +1212,7 @@ ret:
                 src->format = m_parentEnc->m_param->format;
                 if (m_input[view]->readPicture(*src) && !b_ctrl_c)
                 {
+#ifdef ENABLE_ZIMG
                     for (auto &&i : m_cliopt->filters)
                     {
                         i->processFrame(*src);
@@ -1221,6 +1224,7 @@ ret:
                             break;
                         }
                     }
+#endif
 
                     dest->poc = src->poc;
                     dest->pts = src->pts;
