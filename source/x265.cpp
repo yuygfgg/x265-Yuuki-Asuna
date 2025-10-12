cc3104cf75c9f8dbc624cbbad151b78eaae91300
Mr-Z-2697 <74594146+Mr-Z-2697@users.noreply.github.com>
2025-10-13 02:21:51 +0800

    Errors' internalBitDepth promo
    Somehow access this value at the (almost) end will cause invalid pointer read sometimes. Bizarre right?

--- source/x265.cpp
+++ source/x265.cpp
@@ -310,6 +310,7 @@ int main(int argc, char **argv)
     }
 
     int ret = 0;
+    int x265_internalBD = cliopt[0].param->internalBitDepth;
 
     if (cliopt[0].scenecutAwareQpConfig)
     {
@@ -340,7 +341,7 @@ int main(int argc, char **argv)
     }
 
     int totalErrors = numErrorsDuringEncoding;
-    if (cliopt[0].param->internalBitDepth != X265_DEPTH)
+    if (x265_internalBD != X265_DEPTH)
     {
         for (uint8_t idx = 0; idx < numEncodes; idx++)
             totalErrors += cliopt[idx].api->encoder_get_errors();
