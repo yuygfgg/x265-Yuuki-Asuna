eed7e886cae54be9e6e9f7927022378b7d2c4f2f
Mr-Z-2697 <74594146+Mr-Z-2697@users.noreply.github.com>
2025-10-25 16:03:06 +0800

    change hdr10-opt error - it's a warning now.
    the actual behavior has been changed to warning, but I forgot to change the log level.

--- source/encoder/encoder.cpp
+++ source/encoder/encoder.cpp
@@ -4604,7 +4604,7 @@ void Encoder::configure(x265_param *p)
         if (p->internalCsp != X265_CSP_I420 || p->internalBitDepth != 10 || p->vui.colorPrimaries != 9 ||
             p->vui.transferCharacteristics != 16 || p->vui.matrixCoeffs != 9)
         {
-            x265_log(p, X265_LOG_ERROR, "Recommended Settings for HDR10-opt: colour primaries should be BT.2020,\n"
+            x265_log(p, X265_LOG_WARNING, "Recommended Settings for HDR10-opt: colour primaries should be BT.2020,\n"
                                         "                                            transfer characteristics should be SMPTE ST.2084,\n"
                                         "                                            matrix coeffs should be BT.2020,\n"
                                         "                                            the input video should be 10 bit 4:2:0\n"
